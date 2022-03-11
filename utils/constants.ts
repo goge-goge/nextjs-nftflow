2221361db262b5280c4213c9929e31e41ab9d9de
goge-goge <MayXD6@163.com>
2022-03-11 15:05:26 +0800

    first commit

---
+++ utils/constants.ts
@@ -0,0 +1,5 @@
+// const ENDPOINT_ETHEREUM = `https://mainnet.infura.io/v3/${process.env.infuraKey}`;
+const ENDPOINT_RINKEBY = `https://rinkeby.infura.io/v3/${process.env.infuraKey}`;
+// const ENDPOINT_POLYGON = 'https://polygon-rpc.com';
+// const ENDPOINT_MUMBAI = 'https://rpc-mumbai.matic.today';
+export const RPC_URL = ENDPOINT_RINKEBY;
